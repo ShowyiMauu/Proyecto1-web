b71e7f22d29a7d803a5b53aefd812cf149ee5eed
ShowyiMauu <maurygr200714@gmail.com>
2025-03-03 13:08:13 -0600

    Ola maestra, como esta¿?
    trabajko

--- JAVASCRIPT/scripts.js
+++ JAVASCRIPT/scripts.js
@@ -1,5 +1,5 @@
 //obtener el boton por su ID que le puse
-const boton=document.getElementById('botoncito');
+const boton=document.getElementById('botonsito');
 //Agregar un evento al boton
 boton.addEventListener('click',function(){
     alert ("kiubo gente");
